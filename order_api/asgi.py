f8a1f1c5f4266cb42ffd6cb14d5cdfedb0eafd60
hafiz <hafiz.siddiq.umer@gmail.com>
2022-08-12 12:12:31 +0500

    initialized

--- order_api/asgi.py
+++ order_api/asgi.py
@@ -1,16 +1,16 @@
 """
-ASGI config for pets_hospital project.
+ASGI config for order_api project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/4.0/howto/deployment/asgi/
+https://docs.djangoproject.com/en/4.1/howto/deployment/asgi/
 """
 
 import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pets_hospital.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'order_api.settings')
 
 application = get_asgi_application()
--- pets_hospital/router.py
+++
@@ -1,7 +0,0 @@
-from pets.viewsets import   PatientViewset, PatientAppointmentViewset, TotalAmountSumViewSet, UnPaidAppointmentViewSet
-from rest_framework import routers
-
-router = routers.DefaultRouter()
-router.register('patient',PatientViewset, basename='Patient')
-router.register('patient-appointment', PatientAppointmentViewset)
-router.register('unpaid-appointment', UnPaidAppointmentViewSet)
